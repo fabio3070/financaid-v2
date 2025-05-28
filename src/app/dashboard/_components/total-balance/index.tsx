8493cb34e235150eb766d8ff9f6dcab01c8cc829
fabio santos <fabiosantos966@gmail.com>
2025-05-28 10:45:36 +0100

    feat: add folder structure to app routing components to allow testing coupled with components

--- src/app/dashboard/_components/total-balance/index.tsx
+++ src/app/dashboard/_components/total-balance/index.tsx
@@ -2,11 +2,11 @@
 
 import { palette } from '@/lib/palette';
 import React from 'react';
-import TotalBalanceLabel from './total-balance-label';
-import { MonthSelectionBox } from './month-picker';
+import TotalBalanceLabel from './total-balance-label/total-balance-label';
+import { MonthSelectionBox } from './month-picker/month-picker';
 import { useBalance } from '@/hooks/use-balance';
 import { User } from 'next-auth';
-import BalanceBulletTags from './balance-bullet-tags';
+import BalanceBulletTags from './balance-bullet-tags/balance-bullet-tags';
 
 const checkIfBalanceNegative = (balance: number): boolean => {
     return balance < 0;
